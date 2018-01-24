ef8955f5e59f02ab497bc21e1625deda7c0b1ee8
138over <kramerica@icloud.com>
2018-01-24 13:23:00 -0800

    initial

---
+++ react-pivottable/webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    './examples/index.jsx'
+  ],
+  output: {
+    filename: 'bundle.js'
+  },
+  module : {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets" : [["env", {"modules": false}], "react"],
+            "plugins": ["react-hot-loader/babel"]
+          }
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+      contentBase: './examples',
+      hot: true,
+      port: 3000
+    },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
